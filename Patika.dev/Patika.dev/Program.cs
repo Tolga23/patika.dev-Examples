f3cdfcafe53557f50d709a8f2cb9a26efdc08cb1
Tolga23 <thardal23@gmail.com>
2022-03-10 17:38:32 +0300

    patika.dev C# 101 Operatörler
    Operatörler
    Uygulama içerisinden bir atama işlemi yada durum karşılaştırması yapmak istediğimizde operatörleri kullanırız. Bazen koşullara bağlı olarak çalıştırılacak kod bloğunun değişmesi gerekir. Karar yapıları ve operatörler birlikte kullanılarak bu sağlanabilir.
    
    İşlevlerine göre operatörler aşağıdaki gibi kategorilendirilir.
    
    Atama ve İşlemli Atama Operatörleri (=, +=, -=, *=, /=)
    Mantıksal Operatörler (||, &&, !)
    İlişkisel Operatörler (==,!=, <, >, >=,<=)
    Aritmetik (+, -, *, /, %, ++, --)

---
+++ Patika.dev/Patika.dev/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Patika.dev 
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            int totalCost;
+            double meal_cost = 12.00;
+            int tip_percent = 20;
+            int tax_percent = 8;
+            double tip , tax;
+
+
+
+            tip = meal_cost / 100 * tip_percent ;
+                     
+            tax = meal_cost / 100 * tax_percent;
+           
+            totalCost = Convert.ToInt32(meal_cost + tip + tax);
+
+
+            if (totalCost == 15 && tip > 2)
+                Console.WriteLine("Başarılı! \n" + totalCost);
+            else
+                Console.WriteLine("Hata!");
+        }
+
+
+    }
+}
