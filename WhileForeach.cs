a129c9ec08b34c48adcc4b04a45f1a092b76010e
Tolga23 <thardal23@gmail.com>
2022-03-10 22:42:14 +0300

    While ve ForEach Döngüleri

---
+++ WhileForeach.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Patika.dev
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+            // 1 den baslayarak console'dan girilen sayıya kadar ortalama hesaplat
+            Console.Write("Bir sayı girin: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+
+            // a dan z ye kadar tüm harcleri consola yazdır
+            char c = 'a';
+            while (c < 'z')
+            {
+                Console.Write(c);
+                c++;
+            }
+
+            // Foreach
+
+            string[] cars = { "BMW", "Ford", "Toyata" };
+
+            foreach (var car in cars)  
+            {
+                Console.WriteLine(car);
+            }
+
+
+
+
+
+            {
+
+            }
+
+        }
+    }
+}
+
+    
+
