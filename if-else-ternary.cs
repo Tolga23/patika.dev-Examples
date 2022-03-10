86f365ccf17525a91d4749e134e2014a828a469f
Tolga23 <thardal23@gmail.com>
2022-03-10 20:27:21 +0300

    If-Else Yapısı ve Ternary If

---
+++ if-else-ternary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Patika.dev 
+{
+    class Program
+    {
+
+        
+
+static void Main(string[] args)
+        {
+
+            int time = DateTime.Now.Hour;
+
+            if (time>=6 && time <11)
+                Console.WriteLine("Günaydın!");
+
+            else if (time <= 18)
+                Console.WriteLine("İyi günler!");
+            else
+                Console.WriteLine("İyi geceler!");
+         
+            // if-else ternary
+
+            string sonuc = time <= 18 ? "İyi günler" : "İyi geceler";
+            Console.WriteLine(sonuc);
+            sonuc = time <= 6 && time < 11 ? "Günaydın" : time <= 18 ? "İyi günler!!" : "İyi geceler!!";
+            Console.WriteLine(sonuc);
+
+        }
+
+    }
+
+    
+}
