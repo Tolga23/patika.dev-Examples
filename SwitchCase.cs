6c56c33d34c7ec226c6c90fcb9f92351b57c16f7
Tolga23 <thardal23@gmail.com>
2022-03-10 21:12:05 +0300

    Create SwitchCase.cs

---
+++ SwitchCase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Patika.dev 
+{
+    class Program
+    {
+
+        
+
+static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart");
+                    break;
+
+
+                default:
+                break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış ayındasınız");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Bahar ayındasınız");
+                    break;
+
+
+
+                default:
+                    break;
+            }
+        }
+
+    }
+
+    
+}
