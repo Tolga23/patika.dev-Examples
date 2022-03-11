34f2e630468b5a3be6fbb7b824ad45f663f100ff
Tolga23 <thardal23@gmail.com>
2022-03-11 22:52:16 +0300

    Metot & Class

---
+++ MetotClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Patika.dev
+{
+    class Program
+    {
+             
+        static void Main(string[] args)
+        {
+
+            int a = 3, b = 7;
+            Console.WriteLine(Topla(a, b));
+            int sonuc = a + b;
+
+            Methodlar yazdir = new Methodlar();
+            yazdir.EkranaYazdir("Method yazdir: "+sonuc.ToString());
+
+        }
+
+        static int Topla(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+    }
+
+    class Methodlar
+    {
+
+        public void EkranaYazdir(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+    }
+}
+
+    
+
